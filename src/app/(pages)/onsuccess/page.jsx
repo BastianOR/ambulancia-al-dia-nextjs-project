7b97ad8a723cd06e7365b8465819cac4f5239a36
BastianOR <bastianossandon1@gmail.com>
2024-08-02 11:48:26 -0400

    Added an on success page that redirects to /agenda.

--- src/app/(pages)/onsuccess/page.jsx
+++ src/app/(pages)/onsuccess/page.jsx
@@ -1,10 +1,19 @@
 import Link from "next/link";
+import "../../../styles/successpage.css";
+import { todaysDate, todaysMonth } from "../../../lib/dateutils";
+import { CircleCheck } from 'lucide-react';
 
 export default function OnSuccessPage() {
+
+  const today = todaysDate();
+  const tomonth = todaysMonth();
+  const redirectWithQuery = "/agenda?day=" + today + "&month=" + tomonth + "";
+
   return (
     <div className="OnSuccessPage">
-      <h2>Datos ingresados correctamente!</h2>
-      <Link href={"/agenda"}><button>Volver a la agenda</button></Link>
+      <CircleCheck size={90} color="#65a30d" />
+      <h2>Traslado agendado</h2>
+      <Link href={redirectWithQuery}><button>Volver a la agenda</button></Link>
     </div>
   );
 }
